7c8b19bee02d0b16df770974ae0346137160d645
JUAN PABLO RESTREPO JIMENEZ <jprestrepo346@gmail.com>
2024-10-07 06:04:42 -0500

    se quitaron dependencias que no se usaron durante el proyecto

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import {AppDataSource} from "./bd.config";
 import {router as rutaVehiculo} from "./rutas/rutasVehiculo";
 import {router as rutaRegistro} from "./rutas/rutasRegistro";
@@ -12,6 +13,7 @@ const app = express()
 const PUERTO = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan('dev'))
 
 app.use('/vehiculo',rutaVehiculo)
